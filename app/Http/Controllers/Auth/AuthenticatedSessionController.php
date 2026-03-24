b5a2d8daa3e732504efb4f79599644f68afd1ad7
Suliman <salmanyousef13@gmail.com>
2026-03-24 21:13:51 +0300

    all done for week 1:

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -11,9 +11,7 @@ use Illuminate\View\View;
 
 class AuthenticatedSessionController extends Controller
 {
-    /**
-     * Display the login view.
-     */
+    // دالة عرض صفحة تسجيل الدخول
     public function create(): View
     {
         return view('auth.login');
