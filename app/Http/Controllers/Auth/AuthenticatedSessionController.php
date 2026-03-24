017a460a02c368fe09dc0cafcb7335bf8bbce9fe
Suliman <salmanyousef13@gmail.com>
2026-03-24 21:14:13 +0300

    all done for week 1:

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -27,9 +27,7 @@ class AuthenticatedSessionController extends Controller
         return redirect()->intended(route('dashboard', absolute: false));
     }
 
-    /**
-     * Destroy an authenticated session.
-     */
+   
     public function destroy(Request $request): RedirectResponse
     {
         Auth::guard('web')->logout();
