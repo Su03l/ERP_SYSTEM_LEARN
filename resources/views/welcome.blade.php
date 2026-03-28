74f210485542a528e8f8bbc1dad4e72e348e2d28
Suliman <salmanyousef13@gmail.com>
2026-03-28 20:15:01 +0300

    some update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2262,6 +2262,7 @@
                     </li>
                 </ul>
 
+                /* for   */
                 <p class="mt-6 lg:mt-10 text-[#706f6c] dark:text-[#A1A09A]">
                     v{{ app()->version() }}
                     <a href="https://github.com/laravel/laravel/blob/13.x/CHANGELOG.md" target="_blank" class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-1">
