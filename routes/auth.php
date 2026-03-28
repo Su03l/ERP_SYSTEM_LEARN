84d3db3d4dfaf59f2bbc8a9c49bb4f90b9f2f3c0
Suliman <salmanyousef13@gmail.com>
2026-03-28 20:16:23 +0300

    some update

--- routes/auth.php
+++ routes/auth.php
@@ -24,10 +24,10 @@ Route::middleware('guest')->group(function () {
     // لنسيان كلمة المرور
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
         ->name('password.request');
-
+    // لنسيان كلمة المرور
     Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
         ->name('password.email');
-
+    // لنسيان كلمة المرور
     Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
         ->name('password.reset');
 
