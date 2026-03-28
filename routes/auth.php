ad77675ac9ea555ce50828d34e8e45932bb211fe
Suliman <salmanyousef13@gmail.com>
2026-03-28 20:17:04 +0300

    some update

--- routes/auth.php
+++ routes/auth.php
@@ -35,6 +35,7 @@ Route::middleware('guest')->group(function () {
         ->name('password.store');
 });
 
+// يجب ان يكون المستخدم مسجل 
 Route::middleware('auth')->group(function () {
     Route::get('verify-email', EmailVerificationPromptController::class)
         ->name('verification.notice');
