de43943594e1edad67a5b634094a6ec5ca292398
Suliman <salmanyousef13@gmail.com>
2026-03-30 21:19:07 +0300

    some fix

--- app/Enums/TicketStatus.php
+++ app/Enums/TicketStatus.php
@@ -6,4 +6,13 @@ enum TicketStatus: string {
     case OPEN = 'open';
     case IN_PROGRESS = 'in_progress';
     case CLOSED = 'closed';
+
+    public function getLabel(): string
+    {
+        return match($this) {
+            self::OPEN => 'مفتوحة 🟢',
+            self::IN_PROGRESS => 'قيد المعالجة 🟡',
+            self::CLOSED => 'مغلقة 🔴',
+        };
+    }
 }
