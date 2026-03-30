81e31fe52bc91f5347771f374920e736a25a9114
Suliman <salmanyousef13@gmail.com>
2026-03-30 21:46:33 +0300

    some fix

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// النسخ الاحتياطي يومياً الساعة 12 بالليل وتنظيف النسخ القديمة للحفاظ على مساحة السيرفر
+Schedule::command('backup:run')->daily()->at('00:00');
+Schedule::command('backup:clean')->daily()->at('01:00'); 
