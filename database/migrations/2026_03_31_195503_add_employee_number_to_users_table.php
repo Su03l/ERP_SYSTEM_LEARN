d180323af031e0fa8a9e32804dce1355e8678d67
Suliman <salmanyousef13@gmail.com>
2026-03-31 22:55:53 +0300

    some fix

--- database/migrations/2026_03_31_195503_add_employee_number_to_users_table.php
+++ database/migrations/2026_03_31_195503_add_employee_number_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('employee_number')->nullable()->unique()->after('name');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('employee_number');
         });
     }
 };
