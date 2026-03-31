f96809cc5de2d6614913b69cbaa4062b1f68cbbc
Suliman <salmanyousef13@gmail.com>
2026-03-31 22:43:34 +0300

    some fix

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\TicketController;
 use App\Http\Controllers\LeaveRequestController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PayrollController;
+use App\Http\Controllers\PerformanceEvaluationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -25,6 +26,11 @@ Route::middleware('auth')->group(function () {
         Route::resource('employees', EmployeeController::class);
         Route::get('/payroll', [PayrollController::class, 'index'])->name('payroll.index');
         Route::post('/payroll/send', [PayrollController::class, 'sendBulk'])->name('payroll.sendBulk');
+
+        // تقييم الأداء
+        Route::get('/performance-evaluations', [PerformanceEvaluationController::class, 'index'])->name('performance.index');
+        Route::get('/performance-evaluations/lookup', [PerformanceEvaluationController::class, 'lookup'])->name('performance.lookup');
+        Route::post('/performance-evaluations', [PerformanceEvaluationController::class, 'store'])->name('performance.store');
     });
     Route::resource('tickets', TicketController::class)->except(['edit', 'destroy']);
     Route::post('/tickets/{ticket}/comments', [\App\Http\Controllers\TicketCommentController::class, 'store'])->name('ticket.comments.store');
