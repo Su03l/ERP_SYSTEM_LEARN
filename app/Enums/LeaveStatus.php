c1cb453f90b397c6164bf650d42a0742fc2b60d2
Suliman <salmanyousef13@gmail.com>
2026-04-01 17:45:38 +0300

    all done

--- app/Enums/LeaveStatus.php
+++ app/Enums/LeaveStatus.php
@@ -3,7 +3,7 @@
 namespace App\Enums;
 
 enum LeaveStatus: string {
-    case PENDING = 'pending';
+    case PENDING = 'pending'; // الحالة معلفة
     case APPROVED = 'approved';
     case REJECTED = 'rejected';
 }
